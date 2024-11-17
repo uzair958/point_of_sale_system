5a732de26031958cc85c18e790bf014542eeeef4
uzair <uzair.nu2005@gmail.com>
2024-11-17 21:59:25 +0500

    df

--- POS_SYSTEM/settings.py
+++ POS_SYSTEM/settings.py
@@ -105,8 +105,8 @@ if env('RAILWAY_MYSQL_HOST', default=None):
             'NAME': env('MYSQL_DATABASE'),
             'USER': env('MYSQLUSER'),
             'PASSWORD': env('MYSQL_ROOT_PASSWORD'),
-            'HOST': env('RAILWAY_MYSQL_HOST', default=env('host')),  # Correct Railway MySQL host
-            'PORT': env('RAILWAY_MYSQL_PORT', default=env('port')),  # Use the correct port from your env variables
+            'HOST': env('RAILWAY_MYSQL_HOST'),  # Correct Railway MySQL host
+            'PORT': env('RAILWAY_MYSQL_PORT'),  # Use the correct port from your env variables
         }
     }
 else:
@@ -114,9 +114,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': env('MYSQL_DATABASE', default=env('name')),
-            'USER': env('MYSQLUSER', default=env('user')),
-            'PASSWORD': env('MYSQL_ROOT_PASSWORD', default=env('password')),
+            'NAME': env('MYSQL_DATABASE'),
+            'USER': env('MYSQLUSER'),
+            'PASSWORD': env('MYSQL_ROOT_PASSWORD'),
             'HOST': env('RAILWAY_MYSQL_HOST',default=env('host')) , # Default to localhost if not set
             'PORT': env('RAILWAY_MYSQL_PORT', default=env('port')),     # Default MySQL port
         }
