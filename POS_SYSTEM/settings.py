88aa3b335f4398d7bd50fd4f62beade59ad9edc3
Syed Muhammad Uzair Ahmed <140856815+uzair958@users.noreply.github.com>
2024-11-18 20:05:53 +0500

    Update settings.py

--- POS_SYSTEM/settings.py
+++ POS_SYSTEM/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
